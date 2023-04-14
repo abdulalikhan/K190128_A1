ed036e9d6f3308493ab26a0cb9a70e594e1fbb2b
abdulalikhan <k190128@nu.edu.pk>
2023-04-14 12:53:06 +0500

    #18: Implement the project search by tech functionality

---
+++ wwwroot/js/search_filter.js
@@ -0,0 +1,22 @@
+﻿document
+  .getElementById("search-input")
+  .addEventListener("input", filterProjects);
+
+function filterProjects() {
+  const filterValue = document
+    .getElementById("search-input")
+    .value.toLowerCase();
+
+  const projects = document.querySelectorAll(".grid .bg-white");
+
+  projects.forEach((project) => {
+    const tech = project
+      .querySelector("p:nth-child(2)")
+      .textContent.toLowerCase();
+    if (tech.includes(filterValue)) {
+      project.style.display = "block";
+    } else {
+      project.style.display = "none";
+    }
+  });
+}
